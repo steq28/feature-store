aaa6880bb92bf806847a2a1a8b1715372c096ccb
Stefano Quaggio <77461343+steq28@users.noreply.github.com>
2024-07-04 16:05:08 +0200

    create feature group

--- functions/B_create_feature_store.py
+++ functions/B_create_feature_store.py
@@ -6,3 +6,21 @@ def connect_to_hopsworks():
     fs = project.get_feature_store()
 
     return fs
+
+def create_feature_group(name, version, description, primary_key, online_enabled, fs, df, feature_descriptions):
+    fg = fs.get_or_create_feature_group(
+        name=name,
+        version=version,
+        description=description,
+        primary_key=primary_key,
+        online_enabled=online_enabled,
+    )
+
+    # Insert data into feature group
+    fg.insert(df)
+
+    for desc in feature_descriptions: 
+        fg.update_feature_description(desc["name"], desc["description"])
+
+def close_connection(fs):
+    fs.close()
