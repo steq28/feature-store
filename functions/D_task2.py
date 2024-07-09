5d31e76c92028f2476c518527bb111298e495775
Varisck <albyvaro@gmail.com>
2024-07-09 13:36:28 +0200

    task2 refactor

--- functions/D_task2.py
+++ functions/D_task2.py
@@ -10,15 +10,16 @@ from sklearn.svm import LinearSVC
 from functions import A_preprocessing as preprocessing
 import pandas as pd
 
-def test2(wf, wc, ff, fc):
-  print(wc, fc)
-  # df = pd.DataFrame(wf, wc).join(pd.DataFrame(ff, fc))
-  # weather_feature_descriptions.join(flight_airport_feature_descriptions)
-  # df = weather_feature_descriptions
+def svm(X_train, X_test, y_train, y_test):
   supportVec = LinearSVC(dual="auto", tol=1e-5, C=1)
-  X_train, X_test, y_train, y_test = preprocessing.prepare_data_for_ML_model(df, predCol="PRCP")
+  
+  # make task binary
+  y_train = y_train > 0.0
+  y_test = y_test > 0.0
+
   supportVec.fit(X_train, y_train)
   y_pred = supportVec.predict(X_test)
+
   accuracy = accuracy_score(y_test, y_pred)
   print(f"Accuratezza: {accuracy}\n\n")
   print(classification_report(y_test, y_pred))
