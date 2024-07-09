6e18675feb44d9478c336d6bdf387ba06ac9a758
Stefano Quaggio <77461343+steq28@users.noreply.github.com>
2024-07-09 17:07:23 +0200

    deploy

--- functions/deploy_model.py
+++ functions/deploy_model.py
@@ -1,7 +1,8 @@
 from hsml.schema import Schema
 from hsml.model_schema import ModelSchema
+import os
 
-def deploy_model(project, description, X_train, y_train, version, model_name, accuracy):
+def save_model_on_hopsworks(project, description, X_train, y_train, version, model_name, accuracy):
     mr = project.get_model_registry()
 
     # Create input schema using X_train
@@ -29,3 +30,26 @@ def deploy_model(project, description, X_train, y_train, version, model_name, ac
     )
 
     tf_model.save(f'./data/model/{model_name}.pkl')
+
+def deploy_model(project, model_name, model):
+    dataset_api = project.get_dataset_api()
+
+    # Specify the local file path of the Python script to be uploaded
+    local_script_path = "predict_example.py"
+
+    # Upload the Python script to the "Models", and overwrite if it already exists
+    uploaded_file_path = dataset_api.upload(local_script_path, "Models", overwrite=True)
+
+    # Create the full path to the uploaded script for future reference
+    predictor_script_path = os.path.join("/Projects", project.name, uploaded_file_path)
+
+    deployment = model.deploy(
+        name=f"flightdelay{model_name}deployment",  # Specify a name for the deployment
+        script_file=predictor_script_path,  # Provide the path to the Python script for prediction
+    )
+
+    # Print the name of the deployment
+    print("Deployment: " + deployment.name)
+
+    # Display information about the deployment
+    deployment.describe()
